8c092f20c7f026a02e0d7e26c6e89dc1405e8985
Ramesh Nair <ram@hiddentao.com>
2017-03-24 16:48:03 +0000

    eslint test code cleanup

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,36 +1,36 @@
-var path = require('path'),
+const path = require('path'),
   request = require('supertest'),
   shell = require('shelljs'),
-  waigo = require('waigo');
+  waigo = require('waigo')
 
-var test = require('waigo-test-utils').mocha(module);
+const test = require('waigo-test-utils').mocha(module)
 
 
-test['method override'] = {
-  beforeEach: function*() {
+test['integration'] = {
+  beforeEach: function *() {
     var self = this
 
     shell.cp('-Rf', path.join(__dirname, '/../src/support'), this.appFolder)
     shell.cp('-Rf', path.join(__dirname, '/../src/models'), this.appFolder)
 
     yield waigo.init({
-      appFolder: utils.appFolder
+      appFolder: this.appFolder
     })
 
     self.Application = waigo.load('application')
     self.app = self.Application.app
 
-    self.startApp = function*(cfg) {
+    self.startApp = function *(cfg) {
       yield self.Application.start(cfg)
 
       self.request = request(self.app.config.baseURL)
     }
   },
 
-  afterEach: function*() {
+  afterEach: function *() {
     yield this.Application.shutdown()
 
-    shell.rm('-rf', path.join(utils.appFolder, 'support'))
-    shell.rm('-rf', path.join(utils.appFolder, 'models'))
+    shell.rm('-rf', path.join(this.appFolder, 'support'))
+    shell.rm('-rf', path.join(this.appFolder, 'models'))
   }
 }
